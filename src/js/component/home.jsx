54f8201ebed5d7c92e4c972d27379990b6de15d1
Qcohen22 <cohenjr.quincy@live.jobcorps.org>
2022-03-28 18:46:45 +0000

    countup

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,10 +8,16 @@ const Home = () => {
 			setCount((count) => count + 1);
 		}, 1000);
 	}, []);
-
+		let pizza = Math.floor(count / 1000)
+		let football = Math.floor(count / 100)
+		let basketball = Math.floor(count / 10)
+		let credit = Math.floor(count / 1)
 	return (
 		<div>
-			<h1>{count}</h1>
+			<h1>{pizza % 10}</h1>
+			<h1>{football % 10}</h1>
+			<h1>{basketball % 10}</h1>
+			<h1>{credit % 10}</h1>
 		</div>
 	);
 };
